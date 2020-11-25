17c349563aa85dfc58c983dbfa182d0949e74c3e
rafael <rfalconi@unibe.edu.ec>
2020-11-25 14:02:03 -0500

    seleccionar fecha actual para insertar actividad.

--- src/main/java/com/rafael/falconi/example/controllers/ActivityController.java
+++ src/main/java/com/rafael/falconi/example/controllers/ActivityController.java
@@ -5,6 +5,10 @@ import com.rafael.falconi.example.reposotories.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Optional;
 
 @Controller
@@ -16,6 +20,15 @@ public class ActivityController {
     public ActivityController(ActivityRepository activityRepository) {
 
         this.activityRepository = activityRepository;
+    }
+
+    public void createActivity(Activity activity) {
+        LocalDateTime now = LocalDateTime.now();
+        Date date = java.util.Date.from(now.atZone(ZoneId.systemDefault())
+                .toInstant());
+        activity.setDate(date);
+        this.activityRepository.save(activity);
+
     }
 
     public boolean editActivity(Activity activity, int id) {
